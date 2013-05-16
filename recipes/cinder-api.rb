8d9000124c48f431a7322bf1410716e0fe9c084f
Jason Cannavale <jason.cannavale@rackspace.com>
2013-05-15 23:13:34 -0500

    adding cinder monitoring support

---
+++ recipes/cinder-api.rb
@@ -0,0 +1,33 @@
+# Cookbook Name:: openstack-monitoring
+# Recipe:: cinder-api
+#
+# Copyright 2012, Rackspace US, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+include_recipe "monitoring"
+
+if node.recipe?(cinder::cinder-api) or node[:recipes].include?(cinder::cinder-api)
+    platform_options = node[cinder][platform]
+    monitoring_procmon "cinder-api" do
+        service_name=platform_options["cinder_api_service"]
+        process_name "cinder-api"
+        script_name service_name
+    end
+
+    monitoring_metric "cinder-api-proc" do
+        type "proc"
+        proc_name "cinder-api"
+        proc_regex platform_options["cinder_api_service"]
+        alarms(:failure_min => 2.0)
+    end
+end
---
+++ recipes/cinder-volume.rb
@@ -0,0 +1,34 @@
+# Cookbook Name:: openstack-monitoring
+# Recipe:: cinder-volume
+#
+# Copyright 2012, Rackspace US, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+include_recipe "monitoring"
+
+if node.recipe?(keystone::keystone-api) or node[:recipes].include?(keystone::keystone-api)
+    platform_options = node[cinder][platform]
+    monitoring_procmon "cinder-volume" do
+        service_name=platform_options["cinder_volume_service"]
+        process_name "cinder-volume"
+        script_name service_name
+    end
+
+    monitoring_metric "cinder-volume-proc" do
+        type "proc"
+        proc_name "cinder-volume"
+        proc_regex platform_options["cinder_volume_service"]
+        alarms(:failure_min => 2.0)
+    end
+end
+
