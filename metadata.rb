566107134df082d641f8fae9303664f27daf66b4
Jason Cannavale <jason.cannavale@rackspace.com>
2013-05-25 08:19:30 -0500

    fixing typo in metadata

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ description      'Installs/Configures openstack-monitoring'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-%w { centos ubuntu }.each do |os|
+%w{ centos ubuntu }.each do |os|
 	supports os
 end
 
