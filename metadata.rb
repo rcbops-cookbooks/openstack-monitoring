effa18013fa1707f1483e247e337915942c55225
rackerjoe <breu@breu.org>
2013-12-16 08:41:47 -0600

    remove bare tabs

--- metadata.rb
+++ metadata.rb
@@ -6,9 +6,9 @@ long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 version          IO.read(File.join(File.dirname(__FILE__), "VERSION"))
 
 %w{ centos ubuntu }.each do |os|
-	supports os
+  supports os
 end
 
 %w{ keystone monitoring osops-utils }.each do |dep|
-	depends dep
+  depends dep
 end
